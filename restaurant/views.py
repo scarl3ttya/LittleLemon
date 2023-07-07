607750164c1ae03d27c44ad5a7bb75e4f718c16a
yash <yas8830@yahoo.com>
2023-07-07 15:07:40 -0400

    Added index and base templates, and added static files

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('<h1>Hello</h1>')
+def index(request):
+    return render(request, 'index.html', {})
