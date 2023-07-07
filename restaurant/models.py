442fb1b9a9b3550ab0fdc0cbe8d818ef485bb2f8
yash <yas8830@yahoo.com>
2023-07-07 19:01:03 -0400

    Create mysl db and connections. Createed menu and booking models, added them to admin site.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    guest_count = models.SmallIntegerField(default=2)
+    date = models.DateTimeField()
+    
+    def __str__(self): 
+        return self.name
+
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField(default=1)
+
+    def __str__(self): 
+        return self.title
