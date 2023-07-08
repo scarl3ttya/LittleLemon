93cb161482bb96fcc54a3d903094ef08d8fe4394
yash <yas8830@yahoo.com>
2023-07-08 10:36:18 -0400

    Added menu and booking class to views, added serializers for both

---
+++ restaurant/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from . import models
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Booking
+        fields = '__all__'
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Menu
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Menu
+        fields = '__all__'
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, HttpResponse
+from rest_framework.response import Response
+from rest_framework import status, generics, viewsets
+from . import models, serializers
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = models.Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = models.Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+
+class BookingViewset(viewsets.ModelViewSet):
+    queryset = models.Booking.objects.all()
+    serializer_class = serializers.BookingSerializer
+    #permission_classes = [permissions.IsAuthenticated] 
